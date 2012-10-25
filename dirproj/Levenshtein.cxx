7ede09a950713d8e3ea68f3e7f2c15ee8365e45e
perelo <killerperdereau@hotmail.com>
2012-10-25 21:50:36 +0200

    initial commit levenshtein

---
+++ dirproj/Levenshtein.cxx
@@ -0,0 +1,35 @@
+#include <string>
+#include <algorithm>    // min
+
+#include "Levenshtein.h"
+
+using namespace std;
+
+unsigned nsUtil::levenshteinD(const string & MotOrig, const string & MotDest)
+{
+    const string::size_type LgOrig = MotOrig.size();
+    const string::size_type LgDest = MotDest.size();
+
+    if (0 == LgOrig) return LgDest;     // que des insertions
+    if (0 == LgDest) return LgOrig;     // que des suppressions
+
+    // initialisation de la matrice D
+    unsigned D[LgOrig+1][LgDest+1];
+    for (unsigned i(0); i <= LgOrig; ++i) D[i][0] = i;
+    for (unsigned i(1); i <= LgDest; ++i) D[0][i] = i;
+    for (unsigned i(1); i <= LgOrig; ++i)
+        for (unsigned j(1); j <= LgDest; ++j)
+            D[i][j] = 0;
+
+    // remplissage de D
+    for (unsigned i(1); i <= LgOrig; ++i)
+        for (unsigned j(1); j <= LgDest; ++j)
+            D[i][j] = min(min(
+                          D[i-1][j  ] + 1,
+                          D[i  ][j-1] + 1),
+                          D[i-1][j-1] + (MotOrig[i-1] == MotDest[j-1] ? 0
+                                                                      : 1));
+
+    return D[LgOrig][LgDest];
+
+} // levenshteinD()
---
+++ include/Levenshtein.h
@@ -0,0 +1,9 @@
+#include <string>
+
+namespace nsUtil
+{
+
+    unsigned levenshteinD(const std::string & MotOrig,
+                          const std::string & MotDest);
+
+} // namespace nsUtil
