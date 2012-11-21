5930dea1a745dde9a2f3f255847e4e31c9360bd5
Eloi Perdereau <eloi@perdereau.eu>
2012-11-21 13:13:20 +0100

    Compter le temps, osef du hash negatif

--- dirproj/TestHash.cxx
+++ dirproj/TestHash.cxx
@@ -6,17 +6,18 @@
  */
 
 #include <iostream>
-#include <fstream>
 #include <string>
+
+#include <ctime>
 
 using namespace std;
 
 namespace
 {
     int Cap = 1000000;
-    int Hash (string Str)
+    unsigned Hash (const string Str)
     {
-        int Hash (0);
+        unsigned Hash (0);
         for (unsigned i(0); i < Str.size(); ++i)
             Hash = (Hash * 31 + Str[i]) % Cap;
 
@@ -27,16 +28,15 @@ namespace
 
 int main ()
 {
-    ifstream is ("../materiel4/dico_iso.txt");
-    if (!is)
-    {
-        cout << "fail to open ifstream" << endl;
-    }
+    const unsigned Nb (1000000);
 
-    for (string s; getline(is, s); )
-    {
-        if (Hash(s) < 0)
-            cout << "hash de " << s << " negatif" << endl;
-    }
+    clock_t start (clock());
+    for (unsigned i(0); i < 1000000; ++i) Hash("string");
+    float tps ((clock() - start) / float(CLOCKS_PER_SEC) * 1000);
 
-}
+    cout << Nb << " x Jaccard() en " << tps << " ms"
+         << " soit un Jaccard() en " << (tps / Nb) << " ms" << endl;
+
+    return 0;
+
+} // main()
